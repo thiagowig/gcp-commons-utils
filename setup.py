27075e1767f32736db776a5bfc95e162d44d0d1c
Thiago Fonseca <dev.thiago@gmail.com>
2020-07-03 11:45:21 -0300

    Initial commit

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(HERE, "README.md")) as fid:
 # This call to setup() does all the work
 setup(
     name="gcp-commons-utils",
-    version="0.0.1",
+    version="0.0.2",
     description="Utils to be used along Google Cloud Platform components",
     long_description=README,
     long_description_content_type="text/markdown",
